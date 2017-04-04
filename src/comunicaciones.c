670f9e97f0d1c2e5fb1ea86f23ba191fa9744535
Jorge Calatayud Maeso <jorge.cmaeso@alumnos.upm.es>
2017-04-04 17:52:05 +0200

    Fixed Errors
    Pre entrega

--- src/comunicaciones.c
+++ src/comunicaciones.c
@@ -1,108 +1,30 @@
 #include "comunicaciones.h"
 #include <stdio.h>
 #include <stdlib.h>
-
-boolean integridadPaquete(char msg[],int *devolver);
-boolean checkNode(char nodeMessage);
-int checkCmd(char cmd);
-int longitudArray(char msg[]);
-int checkMsg(msg_rcv* msg);
-int aniadeAula(aula* miAula);
-int borraAula(aula* miAula);
-int muestraAula(aula* miAula);
-int muestraTodo(void);
-
-boolean compruebaAula(aula* miAula);
-	aula* getAula(aula* miAula);
-
-
-int hazMsg(aula* miAula,char cmd);
-
-
 #define MY_NODE 0x10
 
-int recPacket(char msg[]) {
-	int devolver = 0;
-	if (integridadPaquete(msg, &devolver) == false) {
-		printf("Paquete Erroneo");
-		return 0;
-	}
-	if(checkNode(msg[0]) == false){
-		printf("No es para mi, pero es valido");
-		return 20;
-	}
-	return devolver;
-}
-
-boolean integridadPaquete(char msg[], int *devolver) {
-	//Probamos a calcular la longitud del array
-	int longArray = longitudArray(msg);
-	if (longArray != -1){
-		//Comprovamos que la info en cmd tenga sentido
-		*devolver = checkCmd(msg[1]);
-		if (*devolver != -1)
-			return true;
-	}
-	return false;
-}
-
-int longitudArray(char msg[]) {
-	int l = 0;
-	while (1) {
-		if (msg[l] == 0x0F && l >= 3 && l < 14) {
-			printf("Paquete Bueno, con final y longitud aceptable( %d )", l+1);
-			return l;
-		}
-		if (msg[l] == 0x0F && l < 3) {
-			printf("Paquete Erroneo, demasiado corto para ser bueno");
-			return -1;
-		}
-		if (l >= 14){
-			printf("Paquete Erroneo, demasiado largo para ser bueno");
-			return -1;
-		}
-		l++;
-	}
-}
-
-boolean checkNode(char nodeMessage) {
-	if (nodeMessage == MY_NODE)
-		return true;
-	return false;
-}
-
-int checkCmd(char cmd) {
-	switch (cmd) {
-	case 'a':
-		return 11;
-	case 'd':
-		return 12;
-	case 'c':
-		return 13;
-	case 'm':
-		return 14;
-	default:
-		printf("CMD Incorrecto");
-		return -1;
-	}
-}
+int checkMsg(msg_rcv* msg);
+int aniadeAula(aula* miAula);
+int borraAula(int id_aula);
+int muestraAula(int id_aula);
+int muestraTodo(void);
+boolean compruebaAula(aula* miAula);
+aula* getAula(int id_aula);
+int hazMsg(msg_rcv* msg);
+int checkAniadeAula(msg_rcv* msg);
+int checkAulaID(msg_rcv* msg);
+void imprimirAula(aula* miAula);
 
 int procesaMsg(msg_rcv* msg){
 	int check = checkMsg(msg);
 	if(check == 0 || check == 20)
 		return check;
-	return hazMsg(msg->dataAula,msg->cmd);
+	return hazMsg(msg);
 }
 
 int checkMsg(msg_rcv* msg){
-	//Check Aula_ID correcto
-	/*if(msg->aula_id != msg->dataAula->aula_id)
-		return 0;*/
-	//Check cmd
-	if(checkCmd(msg->cmd) == -1)
-		return 0;
-	//check aula
-	if((msg->dataAula) == NULL)
+	//Check fin_cmd
+	if(msg->fin_cmd != 0x0F)
 		return 0;
 	//checkNode
 	if(msg->node_ID != MY_NODE)
@@ -110,22 +32,51 @@ int checkMsg(msg_rcv* msg){
 	return 1;
 }
 
-int hazMsg(aula* miAula,char cmd){
-	switch(cmd){
+int hazMsg(msg_rcv* msg){
+	switch(msg->cmd){
 	case 'a':
-		return aniadeAula(miAula);
+		if(checkAniadeAula(msg) == 1){
+			msg->dataAula->aula_id = msg->aula_id;
+			return aniadeAula(msg->dataAula);
+		}
+		return 0;
 	case 'd':
-		return borraAula(miAula);
+		if(checkAulaID(msg) == 1)
+			return borraAula(msg->aula_id);
+		return 0;
 	case 'c':
-		return muestraAula(miAula);
+		if(checkAulaID(msg) == 1)
+			return muestraAula(msg->aula_id);
+		return 0;
 	case 'm':
 		return muestraTodo();
 	default:
 		printf("Mensaje incorrecto");
-		printf("NO DEBERIA ESTAR AQUI");
+		printf("El CMD Recivido no es valido");
 		return 0;
 	}
 }
+
+int checkAniadeAula(msg_rcv* msg){
+	if(msg->aula_id < 0){
+		printf("Error de ID negativo \n");
+		return 0;
+	}
+	if(msg->dataAula == NULL){
+		printf("Error dataAula es NULL \n");
+		return 0;
+	}
+	return 1;
+}
+
+int checkAulaID(msg_rcv* msg){
+	if(msg->aula_id < 0){
+		printf("Error de ID negativo \n");
+		return 0;
+	}
+	return 1;
+}
+
 
 int aniadeAula(aula* miAula){
 	//Comprovamos que cumple las condiciones de no repeticion
@@ -157,48 +108,57 @@ int aniadeAula(aula* miAula){
 	nAulas++;
 	return 21;
 }
-int muestraAula(aula* miAula){
+int muestraAula(int id_aula){
 	//Comprovamos que el aula está registrada y obtenermos un puntero
-	aula* aulaMostrar = getAula(miAula);
+	aula* aulaMostrar = getAula(id_aula);
 	if(aulaMostrar != NULL){
 		imprimirAula(aulaMostrar);
 		return 23;
 	}
 	return -1;
 }
-int borraAula(aula* miAula){
+int borraAula(int id_aula){
 	//Comprovamos que el aula está registrada y obtenermos un puntero
 	aula* listaAulas = NULL;
 	aula* aulaBorrar = NULL;
 	int num = 0;
 	//Hacemos una estructura paralela en la que almacenamos lo que no borramos
 	for(int i = 0; i < nAulas; i++){
-		if(miAula->aula_id != datosAulas[i].aula_id){
+		if(id_aula != datosAulas[i].aula_id){
 			//NOTA: HACEMOS UN ALLOC DIRECTAMENTE PORQUE INICIALIZAMOS EL PUNT
+
+			listaAulas = realloc(listaAulas,(num+1)*sizeof(aula));
+			listaAulas[num].aula_id = datosAulas[i].aula_id;
+			listaAulas[num].dni = datosAulas[i].dni;
+			listaAulas[num].luminosidad = datosAulas[i].luminosidad;
+			listaAulas[num].presencia = datosAulas[i].presencia;
+			listaAulas[num].temperatura = datosAulas[i].temperatura;
 			num++;
-			listaAulas = realloc(listaAulas,num*sizeof(aula));
 		}else{
 			aulaBorrar = datosAulas+i;
 		}
 	}
 	//Verificamos existe un valor que coincide con el parametro
-	if(!aulaBorrar && num == (nAulas-1)){
+	if(num == (nAulas-1)){
+		free(datosAulas);
 		datosAulas = listaAulas;
 		free(aulaBorrar);
 		nAulas = num;
 		return 22;
 	}else{
-		printf("Error no se ha podido borrar");
+		printf("Error no se ha podido borrar\n");
 		return -1;
 	}
 }
 int muestraTodo(void){
+	aula* miAula = NULL;
 	if(nAulas == 0){
-		printf("No hay datos que imprimir");
+		printf("No hay datos que imprimir \n");
 		return -1;
 	}
 	for(int i = 0; i < nAulas; i++){
-		imprimirAula(datosAulas[i]);
+		miAula = datosAulas+i;
+		imprimirAula(miAula);
 	}
 	return 24;
 }
@@ -211,12 +171,12 @@ void imprimirAula(aula* miAula){
 	printf("\tdni:\t%s\n",miAula->dni);
 	return;
 }
-aula* getAula(aula* miAula){
+aula* getAula(int id_aula){
 	if(nAulas == 0 || datosAulas == NULL)
 		return NULL;
 	aula* aulaDevolver = NULL;
 	for(int i = 0; i < nAulas; i++){
-		if(datosAulas[i].aula_id == miAula->aula_id)
+		if(datosAulas[i].aula_id == id_aula)
 			aulaDevolver = datosAulas+i;
 			return aulaDevolver;
 	}
--- src/test.c
+++ src/test.c
@@ -5,16 +5,19 @@
  *      Author: Elena
  */
 
-
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "comunicaciones.h"
 #include "tipos.h"
 
+msg_rcv* generadorMsg(int aula_id, char cmd, int aula);
+aula* generadorAula();
 
 int main (){
 	int result;
 	nAulas = 0;
+	srand(time(NULL));
 
 	aula* aula1 = (aula*)malloc(sizeof(aula));
 	aula1->temperatura = 22;
@@ -24,7 +27,7 @@ int main (){
 
 	msg_rcv* mssg1 = (msg_rcv*)malloc(sizeof(msg_rcv));
 	mssg1->node_ID = 0x10;
-	mssg1->aula_id = 3;
+	mssg1->aula_id = -1;
 	mssg1->cmd = 'a';
 	mssg1->dataAula = aula1;
 	mssg1->fin_cmd = 0x0F;
@@ -37,7 +40,101 @@ int main (){
 	printf("procesaMsg devuelve %d\n", result);
 
 	free(aula1);
-	free (mssg1);
+	free(mssg1);
 
+	msg_rcv* msg0 = generadorMsg(0,'a',1);
+	msg_rcv* msg1 = generadorMsg(1,'a',1);
+	msg_rcv* msg2 = generadorMsg(2,'a',1);
+	msg_rcv* msg3 = generadorMsg(3,'a',1);
+	msg_rcv* msg32 = generadorMsg(3,'a',1);
+	msg_rcv* msg3e = generadorMsg(3,'a',0);
+	msg_rcv* msgd = generadorMsg(2,'d',0);
+	msg_rcv* msgd2 = generadorMsg(2,'d',0);
+	msg_rcv* msgd3 = generadorMsg(1,'d',0);
+	msg_rcv* msgce = generadorMsg(2,'c',0);
+	msg_rcv* msgc = generadorMsg(0,'c',0);
+	msg_rcv* msgm = generadorMsg(2,'m',0);
+
+	result = procesaMsg(msg0);
+	printf("procesaMsg devuelve %d(deberia ser 21)\n", result);
+
+	result = procesaMsg(msg1);
+	printf("procesaMsg1 devuelve %d(deberia ser 21)\n", result);
+
+	result = procesaMsg(msg2);
+	printf("procesaMsg2 devuelve %d(deberia ser 21)\n", result);
+
+	result = procesaMsg(msg3);
+	printf("procesaMsg3 devuelve %d(deberia ser 21)\n", result);
+
+	result = procesaMsg(msg32);
+	printf("procesaMsg32 devuelve %d(deberia ser -1)\n", result);
+
+	result = procesaMsg(msg3e);
+	printf("procesaMsg3e devuelve %d(deberia ser 0)\n", result);
+
+	result = procesaMsg(msgd);
+	printf("procesaMsgd devuelve %d(deberia ser 22)\n", result);
+
+	result = procesaMsg(msgd2);
+	printf("procesaMsgd2 devuelve %d(deberia ser -1)\n", result);
+
+	result = procesaMsg(msgd3);
+	printf("procesaMsgd3 devuelve %d(deberia ser 22)\n", result);
+
+	result = procesaMsg(msgce);
+	printf("procesaMsgc devuelve %d(deberia ser -1)\n", result);
+
+	result = procesaMsg(msgc);
+	printf("procesaMsgc devuelve %d(deberia ser 23)\n", result);
+
+	result = procesaMsg(msgm);
+	printf("procesaMsgm devuelve %d(deberia ser 24)\n", result);
+
+	//free(msg0->aula_id);
+	free(msg0);
+	//free(msg1->aula_id);
+	free(msg1);
+	//free(msg2->aula_id);
+	free(msg2);
+	//free(msg3->aula_id);
+	free(msg3);
+	//free(msg32->aula_id);
+	free(msg32);
+	//free(msg3e->aula_id);
+	free(msg3e);
+	free(msgd);
+	free(msgd2);
+	//free(msgd->aula_id);
+	free(msgce);
+	free(msgc);
+	//free(msgm->aula_id);
+	free(msgm);
 	return 0;
 }
+
+msg_rcv* generadorMsg(int aula_id, char cmd, int aula){
+	msg_rcv* msg = (msg_rcv*)malloc(sizeof(msg_rcv));
+	msg->node_ID = 0x10;
+	msg->aula_id = aula_id;
+	msg->cmd = cmd;
+	if(aula == 1)
+		msg->dataAula = generadorAula();
+	else
+		msg->dataAula = NULL;
+	msg->fin_cmd = 0x0F;
+	return msg;
+}
+aula* generadorAula(){
+	aula* miAula = (aula*)malloc(sizeof(aula));
+	miAula->temperatura = rand()%55;
+	miAula->luminosidad = rand()%100;
+	if((rand()%2) == 1)
+		miAula->presencia = 's';
+	else
+		miAula->presencia = 'n';
+	miAula->dni = "50555444K 50444555S 50334455E";
+	return miAula;
+}
+
+
